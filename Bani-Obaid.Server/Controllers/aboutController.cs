a73de2cd8fd66295a3786e9dc88dbfa4769ddb67
dimamohamad <deemaahmad189@gmail.com>
2024-11-03 16:17:06 +0300

    add controller

---
+++ Bani-Obaid.Server/Controllers/aboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bani_Obaid.Server.Controllers
+{
+    public class aboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

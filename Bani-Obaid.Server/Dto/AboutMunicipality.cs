e94a322c0875802fd184aa9f440ad4c3a341157f
ahmadqadomi-1 <qadomia95@gmail.com>
2024-11-14 03:20:49 +0300

    Add about_municipalityController

---
+++ Bani-Obaid.Server/Dto/AboutMunicipality.cs
@@ -0,0 +1,13 @@
+﻿namespace Bani_Obaid.Server.Dto
+{
+    public class AboutMunicipality
+    {
+        public string Description { get; set; } = null!;
+
+        public IFormFile? DescriptionImage { get; set; } = null!;
+
+        public string Vision { get; set; } = null!;
+
+        public string Mission { get; set; } = null!;
+    }
+}

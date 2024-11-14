02553acace8af1ca55e616fb7d89db5c5e0250c2
dimamohamad <deemaahmad189@gmail.com>
2024-11-14 12:43:52 +0300

    edit table

---
+++ Bani-Obaid.Server/Models/PollVote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bani_Obaid.Server.Models;
+
+public partial class PollVote
+{
+    public int Id { get; set; }
+
+    public int? PollTopicId { get; set; }
+
+    public int? NationalId { get; set; }
+
+    public string? Message { get; set; }
+
+    public int? VoteRate { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual PollTopic? PollTopic { get; set; }
+}

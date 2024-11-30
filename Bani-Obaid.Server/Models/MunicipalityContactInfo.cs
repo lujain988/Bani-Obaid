f38751843a621a3a1d5dd4da9ce68bf20230eea6
dimamohamad <deemaahmad189@gmail.com>
2024-11-30 21:54:05 +0300

    edit database

---
+++ Bani-Obaid.Server/Models/MunicipalityContactInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bani_Obaid.Server.Models;
+
+public partial class MunicipalityContactInfo
+{
+    public int Id { get; set; }
+
+    public string? Logo { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Facebook { get; set; }
+
+    public string? Youtube { get; set; }
+
+    public string? Twitter { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
